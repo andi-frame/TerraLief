1a710a565219cb2587302ea613df851ef2a0f36d
andi-frame <andifarhan1094@gmail.com>
2026-03-08 14:01:04 +0700

    chore: add allowedHosts to vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["terralief.byhan.id", "localhost", "127.0.0.1", "0.0.0.0", "::1"],
+    port: 5173,
+    host: "0.0.0.0",
+  }
 })
